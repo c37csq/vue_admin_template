80f5ca83729661f0a39b0bfa8d8804a367ea168f
陈思琪 <1903498134@qq.com>
2022-04-13 09:13:36 +0800

    chore: update packages

--- components.d.ts
+++ components.d.ts
@@ -16,11 +16,8 @@ declare module 'vue' {
     ElAvatar: typeof import('element-plus/es')['ElAvatar'];
     ElButton: typeof import('element-plus/es')['ElButton'];
     ElCard: typeof import('element-plus/es')['ElCard'];
-    ElCollapse: typeof import('element-plus/es')['ElCollapse'];
-    ElCollapseItem: typeof import('element-plus/es')['ElCollapseItem'];
     ElConfigProvider: typeof import('element-plus/es')['ElConfigProvider'];
     ElContainer: typeof import('element-plus/es')['ElContainer'];
-    ElDatePicker: typeof import('element-plus/es')['ElDatePicker'];
     ElDialog: typeof import('element-plus/es')['ElDialog'];
     ElDropdown: typeof import('element-plus/es')['ElDropdown'];
     ElDropdownItem: typeof import('element-plus/es')['ElDropdownItem'];
@@ -28,7 +25,6 @@ declare module 'vue' {
     ElForm: typeof import('element-plus/es')['ElForm'];
     ElFormItem: typeof import('element-plus/es')['ElFormItem'];
     ElHeader: typeof import('element-plus/es')['ElHeader'];
-    ElIcon: typeof import('element-plus/es')['ElIcon'];
     ElInput: typeof import('element-plus/es')['ElInput'];
     ElMain: typeof import('element-plus/es')['ElMain'];
     ElMenu: typeof import('element-plus/es')['ElMenu'];
@@ -36,14 +32,11 @@ declare module 'vue' {
     ElOption: typeof import('element-plus/es')['ElOption'];
     ElPagination: typeof import('element-plus/es')['ElPagination'];
     ElRadio: typeof import('element-plus/es')['ElRadio'];
-    ElRadioButton: typeof import('element-plus/es')['ElRadioButton'];
     ElRadioGroup: typeof import('element-plus/es')['ElRadioGroup'];
     ElScrollbar: typeof import('element-plus/es')['ElScrollbar'];
     ElSelect: typeof import('element-plus/es')['ElSelect'];
     ElSkeleton: typeof import('element-plus/es')['ElSkeleton'];
     ElSkeletonItem: typeof import('element-plus/es')['ElSkeletonItem'];
-    ElStep: typeof import('element-plus/es')['ElStep'];
-    ElSteps: typeof import('element-plus/es')['ElSteps'];
     ElSubMenu: typeof import('element-plus/es')['ElSubMenu'];
     ElTable: typeof import('element-plus/es')['ElTable'];
     ElTableColumn: typeof import('element-plus/es')['ElTableColumn'];
